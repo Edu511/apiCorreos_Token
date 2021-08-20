970d3ef4baae29602610468fb9f06ad6a2459233
Jose Eduardo <jeduardo180895@gmail.com>
2021-08-20 11:14:01 -0500

    Agregar archivos de proyecto.

---
+++ apiCorreos/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace apiCorreos.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ apiCorreos/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace apiCorreos.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
